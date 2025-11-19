d0ba3ffb1fd3ec064f33446f4260ac3e624b12c4
Salah Mel-Catherine Atuma <melcatherinesalah@gmail.com>
2025-11-19 15:08:21 +0100

    Revert "setting up folder structure for users and search history. installing MongoSB and Mongoose. Setting up environmental variables (Mongo_URI, Port). Designing a user collection schema"
    This reverts commit 06b8a98dec7f3ec69a9644c1b6527f63291b1162.

--- src/search-history/search-history.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-@Controller("search-history")
-export class SearchHistoryController {}
--- src/search-history/search-history.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { SearchHistoryController } from "./search-history.controller";
-import { SearchHistoryService } from "./search-history.service";
-
-@Module({
-	controllers: [SearchHistoryController],
-	providers: [SearchHistoryService],
-})
-export class SearchHistoryModule {}

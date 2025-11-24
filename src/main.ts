015d0be2ce21fc2342d4ca78096f1a953e1e388d
Kongnyu Roger <kongnyuroger711@gmail.com>
2025-11-24 09:50:48 +0100

    included cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: ["http://localhost:3000", "http://192.168.1.53:3000"],
+		credentials: true,
+		methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+		allowedHeaders: "Content-Type, Authorization",
+	});
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
